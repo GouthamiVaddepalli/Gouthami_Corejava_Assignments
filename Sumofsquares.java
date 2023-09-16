6820dcab61cf0b4a1942799c387a614dbb2a62e5
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-09-16 09:00:45 +0530

    Update Sumofsquares.java

--- Sumofsquares.java
+++ Sumofsquares.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class Sumofsquares {
 
-	public int calculateDifference(int num) {
+	public static int calculateDifference(int n) {
+
+		public static int calculateDifference(int n) {
 
 		int sumofsquares = 0;
-		int squareofsum = 0;
 		int sum = 0;
 
+		int squareofsum = 0;
 		int squares;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter n value : ");
-		int n = scan.nextInt();
 
 		for (int i = 1; i <= n; i++) {
 			squareofsum = squareofsum + i;
@@ -21,20 +20,22 @@ public class Sumofsquares {
 			sum = sum + sumofsquares;
 		}
 
-		System.out.println(sum);
+		System.out.println("square of natural num : " + sum);
 
-		System.out.println(squareofsum);
+		System.out.println("sum of natural num : " + squareofsum);
 		squares = (squareofsum) * (squareofsum);
-		System.out.println(squares);
+		System.out.println("square of sum : " + squares);
 		int difference = sum - squares;
 		return difference;
 	}
 
 	public static void main(String[] args) {
 
-		Sumofsquares obj = new Sumofsquares();
-		int num = 0;
-		System.out.println(obj.calculateDifference(num));
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter n value : ");
+		int n = scan.nextInt();
+		System.out.println("Difference : " + calculateDifference(n));
+
 
 	}
 
