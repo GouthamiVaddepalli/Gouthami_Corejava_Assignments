266d1b65f22c9ff26a71e2a11edd0e087040054b
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-09-16 11:22:12 +0530

    Update Sumofsquares.java

--- Sumofsquares.java
+++ Sumofsquares.java
@@ -2,28 +2,28 @@ package com.variables;
 
 import java.util.Scanner;
 
-public class Sumofsquares {
+public class SumOfSquares {
 
 	public static int calculateDifference(int n) {
 
 		public static int calculateDifference(int n) {
 
-		int sumofsquares = 0;
+		int sumOfSquares = 0;
 		int sum = 0;
 
-		int squareofsum = 0;
+		int squareOfSum = 0;
 		int squares;
 
 		for (int i = 1; i <= n; i++) {
-			squareofsum = squareofsum + i;
-			sumofsquares = i * i;
-			sum = sum + sumofsquares;
+			squareOfSum = squareOfSum + i;
+			sumOfSquares = i * i;
+			sum = sum + sumOfSquares;
 		}
 
 		System.out.println("square of natural num : " + sum);
 
-		System.out.println("sum of natural num : " + squareofsum);
-		squares = (squareofsum) * (squareofsum);
+		System.out.println("sum of natural num : " + squareOfSum);
+		squares = (squareOfSum) * (squareOfSum);
 		System.out.println("square of sum : " + squares);
 		int difference = sum - squares;
 		return difference;
